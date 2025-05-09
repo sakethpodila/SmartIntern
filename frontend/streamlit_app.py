bf0e8f0b1c39dea836072201b303afe09c1579a2
KSAP02 <ksap.pradyumn@gmai.com>
2025-05-09 19:23:55 +0100

    Changed the bakend url in streamlit_app.py file and commented out enVars in render.yaml file

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -5,7 +5,7 @@ import requests
 
 # BACKEND_URL = "http://127.0.0.1:8000"  # Replace with your backend URL
 # BACKEND_URL = "http://backend:8000"
-BACKEND_URL = "https://backend-2cxc.onrender.com"  # Replace with your backend URL
+BACKEND_URL = "https://backend-gd2l.onrender.com"  # Replace with your backend URL
 
 st.set_page_config(
     page_title = "SmartIntern",
