18617680c083d0d08b78d2762284883eeaf4e774
KSAP02 <ksap.pradyumn@gmai.com>
2025-05-09 17:07:14 +0100

    Changed the backend url to match deployed url in both sreamlit_app.py and render.yaml files

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -4,7 +4,8 @@ from datetime import datetime
 import requests
 
 # BACKEND_URL = "http://127.0.0.1:8000"  # Replace with your backend URL
-BACKEND_URL = "http://backend:8000"
+# BACKEND_URL = "http://backend:8000"
+BACKEND_URL = "https://backend-2cxc.onrender.com"  # Replace with your backend URL
 
 st.set_page_config(
     page_title = "SmartIntern",
